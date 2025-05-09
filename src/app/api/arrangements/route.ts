0452d440f61b9071c9d8470f236032a8bc76f906
Nat1anWasTaken <nathan@nat1an.xyz>
2025-05-10 02:13:18 +0800

    feat(api/arrangements): add arrangement creation with visibility, title, and composers

--- src/app/api/arrangements/route.ts
+++ src/app/api/arrangements/route.ts
@@ -1,12 +1,33 @@
-import { NextRequest } from "next/server";
+import { Visibility } from "@/generated/prisma";
+import { auth0 } from "@/lib/auth0";
 import { prisma } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
-interface CreateArrangementBody {}
+interface CreateArrangementBody {
+  visibility: Visibility;
+  title: string;
+  composers: string[];
+  arrangementType: string;
+}
 
-export async function POST(request: NextRequest) {
-  prisma.arrangement.create({
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const body = (await request.json()) as CreateArrangementBody;
+
+  const arrangement = await prisma.arrangement.create({
     data: {
-      visibility: "private"
+      visibility: body.visibility,
+      title: body.title,
+      composers: body.composers.map((c) => c.trim()),
+      arrangement_type: body.arrangementType,
+      uploaded_by: session.user.sub
     }
   });
+
+  return NextResponse.json(arrangement, { status: 200 });
 }
