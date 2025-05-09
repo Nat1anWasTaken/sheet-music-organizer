e943b7f76d81d0fc4e686c0fa84a01bb24603e28
Nat1anWasTaken <nathan@nat1an.xyz>
2025-05-10 02:13:39 +0800

    fix(components/upload/dialogs/confirm-upload-dialog): correct variable name for file upload context

--- src/components/upload/dialogs/confirm-upload-dialog.tsx
+++ src/components/upload/dialogs/confirm-upload-dialog.tsx
@@ -6,9 +6,9 @@ import { UploadTrayContext } from "@/components/upload/upload-tray";
 export function ConfirmUploadDialog(props: { dialog: UseDialogReturn; confirmCallback: () => void }) {
   const { dialog, confirmCallback } = props;
 
-  const fileUpload = useContext(UploadTrayContext);
+  const fileUploadTray = useContext(UploadTrayContext);
 
-  if (!fileUpload) {
+  if (!fileUploadTray) {
     throw new Error("FileUpload context is not available, perhaps you forgot to wrap the component with UploadTrayContext.Provider?");
   }
 
@@ -18,7 +18,7 @@ export function ConfirmUploadDialog(props: { dialog: UseDialogReturn; confirmCal
         <Dialog.Title>Are you sure you want to upload these files?</Dialog.Title>
       </Dialog.Header>
       <Dialog.Body>
-        <FileUpload.RootProvider value={fileUpload}>
+        <FileUpload.RootProvider value={fileUploadTray}>
           <Box overflowY={"scroll"} maxHeight={"lg"}>
             <FileUpload.Context>
               {(context) =>
