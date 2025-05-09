c38f9005f78a19889f8d02fe2c50e111387b3b16
Nat1anWasTaken <nathan@nat1an.xyz>
2025-05-10 02:13:55 +0800

    feat(components/upload/upload-tray): refactor upload tray to improve file handling and integrate toaster notifications

--- src/components/upload/upload-tray.tsx
+++ src/components/upload/upload-tray.tsx
@@ -1,18 +1,19 @@
 "use client";
-import { Box, useDialog, useFileUpload, UseFileUploadReturn, VStack } from "@chakra-ui/react";
-import Dropzone from "@/components/upload/dropzone";
-import { createContext } from "react";
-import { useMetadataGenerator } from "@/hooks/use-metadata-generator";
-import MetadataForm from "@/components/upload/metadata-form";
 import { ConfirmGenerateDialog } from "@/components/upload/dialogs/confirm-generate-dialog";
-import { useMetadataForm } from "@/hooks/use-metadata-form";
-import { GenerateOrUpload } from "@/components/upload/generate-or-upload";
 import { ConfirmUploadDialog } from "@/components/upload/dialogs/confirm-upload-dialog";
+import Dropzone from "@/components/upload/dropzone";
+import { GenerateOrUpload } from "@/components/upload/generate-or-upload";
+import MetadataForm from "@/components/upload/metadata-form";
+import { useMetadataForm } from "@/hooks/use-metadata-form";
+import { useMetadataGenerator } from "@/hooks/use-metadata-generator";
+import { Box, useDialog, useFileUpload, UseFileUploadReturn, VStack } from "@chakra-ui/react";
+import { createContext } from "react";
+import { toaster } from "../ui/toaster";
 
 export const UploadTrayContext = createContext<UseFileUploadReturn | null>(null);
 
 export default function UploadTray() {
-  const fileUpload = useFileUpload({
+  const fileUploadTray = useFileUpload({
     maxFiles: 20,
     maxFileSize: 100 * 1024 * 1024,
     accept: "application/pdf"
@@ -24,7 +25,7 @@ export default function UploadTray() {
   const metadataForm = useMetadataForm();
 
   const { generationStatus, generateMetadata } = useMetadataGenerator({
-    files: fileUpload.acceptedFiles,
+    files: fileUploadTray.acceptedFiles,
     onSuccess: (data) => {
       metadataForm.setFormContent({
         title: data.title,
@@ -33,21 +34,37 @@ export default function UploadTray() {
       });
     },
     fileSplitCallback: (newFiles) => {
-      fileUpload.acceptedFiles.splice(0, fileUpload.acceptedFiles.length, ...newFiles);
+      fileUploadTray.acceptedFiles.splice(0, fileUploadTray.acceptedFiles.length, ...newFiles);
     }
   });
 
   const uploadFiles = () => {
-    fetch("/api/upload");
+    fetch("/api/arrangements", {
+      method: "POST",
+      headers: {},
+      body: JSON.stringify({
+        visibility: "public", // TODO: make this dynamic
+        title: metadataForm.formContent.title,
+        composers: metadataForm.formContent.composers.split(",").map((c) => c.trim()),
+        arrangementType: metadataForm.formContent.arrangementType
+      })
+    }).then((response) => {
+      if (response.ok) {
+        toaster.create({
+          type: "success",
+          description: "Files uploaded successfully"
+        });
+      }
+    });
   };
 
   return (
-    <UploadTrayContext.Provider value={fileUpload}>
+    <UploadTrayContext.Provider value={fileUploadTray}>
       <Box width={{ base: "full", md: "xl" }} border={"solid"} borderWidth={"1px"} borderColor={"border"} p={4} rounded={"md"}>
         <VStack gap={4}>
-          <Dropzone fileUpload={fileUpload} />
+          <Dropzone fileUpload={fileUploadTray} />
 
-          {fileUpload.acceptedFiles.length > 0 ? (
+          {fileUploadTray.acceptedFiles.length > 0 ? (
             <>
               <VStack>
                 <MetadataForm value={metadataForm} />
