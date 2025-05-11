266c8a71a10743fa98fed5ac8e8f2047599e2ca8
Nat1anWasTaken <nathan@nat1an.xyz>
2025-05-11 21:07:06 +0800

    feat(components/upload/generate-or-upload): add upload status handling and integrate loading states for upload button

--- src/components/upload/generate-or-upload.tsx
+++ src/components/upload/generate-or-upload.tsx
@@ -1,15 +1,17 @@
 import { GenerationStatus } from "@/hooks/use-metadata-generator";
+import { UploadStatus } from "@/hooks/use-upload-arrangement";
 import { Button, HStack } from "@chakra-ui/react";
 import { useMemo } from "react";
 
 interface GenerateOrUploadProps {
   generationStatus: GenerationStatus;
   generateCallback?: () => void;
+  uploadStatus: UploadStatus;
   uploadCallback?: () => void;
 }
 
 export function GenerateOrUpload(props: GenerateOrUploadProps) {
-  const { generationStatus, generateCallback, uploadCallback } = props;
+  const { generationStatus, generateCallback, uploadStatus, uploadCallback } = props;
 
   const generationStatusText = useMemo(() => {
     return {
@@ -20,18 +22,26 @@ export function GenerateOrUpload(props: GenerateOrUploadProps) {
     }[generationStatus];
   }, [generationStatus]);
 
-  const isGenerating = useMemo(() => [
-    GenerationStatus.MergingFile,
-    GenerationStatus.Uploading,
-    GenerationStatus.WaitingForResponse
-  ].includes(generationStatus), [generationStatus]);
+  const isGenerating = useMemo(() => [GenerationStatus.MergingFile, GenerationStatus.Uploading, GenerationStatus.WaitingForResponse].includes(generationStatus), [generationStatus]);
+
+  const uploadStatusText = useMemo(() => {
+    return {
+      [UploadStatus.Idle]: "Upload",
+      [UploadStatus.CreatingArrangement]: "Creating arrangement...",
+      [UploadStatus.UploadingParts]: "Uploading..."
+    }[uploadStatus];
+  }, [uploadStatus]);
+
+  const isUploading = useMemo(() => [UploadStatus.CreatingArrangement, UploadStatus.UploadingParts].includes(uploadStatus), [uploadStatus]);
 
   return (
     <HStack gap={"2"} justify={"flex-end"}>
       <Button variant={"subtle"} loading={isGenerating} loadingText={generationStatusText} onClick={generateCallback}>
         {generationStatusText}
       </Button>
-      <Button onClick={uploadCallback}>Upload</Button>
+      <Button loading={isUploading} loadingText={uploadStatusText} onClick={uploadCallback}>
+        Upload
+      </Button>
     </HStack>
   );
 }
