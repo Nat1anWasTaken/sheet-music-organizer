b355ed837c10e1899e7aefa5a41db56a9e89c725
Nat1anWasTaken <nathan@nat1an.xyz>
2025-05-13 12:59:31 +0800

    refactor(upload/loading.tsx): replace skeleton loading with spinner and text

--- src/app/upload/loading.tsx
+++ src/app/upload/loading.tsx
@@ -1,12 +1,13 @@
-import { Box, Flex, Icon, Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import { Box, Flex, Spinner, Text } from "@chakra-ui/react";
 
 export default function LoadingUploadPage() {
   return (
     <Flex padding={"4"} width={"full"} h={"full"} alignItems={"center"} justifyContent={"center"} direction={"column"} gap={"4"} overflowY={"auto"}>
       <Box width={{ base: "full", md: "xl" }} border={"solid"} borderWidth={"1px"} borderColor={"border"} p={4} rounded={"md"}>
-        <Box minH={"2xs"} maxW={"xl"}>
-          <Skeleton />
-        </Box>
+        <Flex minH={"2xs"} maxW={"xl"} justifyContent={"center"} alignItems={"center"} direction={"row"} gap={4}>
+          <Spinner size={"lg"} />
+          <Text>Loading...</Text>
+        </Flex>
       </Box>
     </Flex>
   );
